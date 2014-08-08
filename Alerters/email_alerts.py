9aec369b5abb24506776d7f43c80e575132ea2d0
Jesse <jelloeater@gmail.com>
2014-08-08 15:55:38 -0400

    Almost done with report generation (going to change format from list to table)

--- Alerters/email_alerts.py
+++ Alerters/email_alerts.py
@@ -35,44 +35,52 @@ class email_actions():
     @staticmethod
     def generate_report():
         """ Created report of all fails since last email was sent """
-        fail_list = db_helpers.server_stats.failures_in_x_minutes_ago(db_helpers.email_log.email_sent_x_minutes_ago())
-        server_list = db_helpers.monitor_list.get_server_list()
 
-        fail_list_txt = []
+        def generate_fail_list(fail_list_in, monitor_list_in):
 
-        for i in fail_list:
-            host_name = None
-            try:
-                host_name = str(i[2]).split(':')[0]
-                port = str(i[2]).split(':')[1]
-                fail_list_txt.append('\nHost: ' + host_name)
-                fail_list_txt.append('\nPort: ' + port)
-            except IndexError:
-                # For Hosts with no port
-                host_name = str(i[2])
-                fail_list_txt.append('\nHost: ' + host_name)
+            fail_list_txt = []
+            for i in fail_list_in:
+                try:
+                    # Try to parse port from hostname address
+                    # (should still work even if host is removed from monitor_list)
+                    host_name = str(i[2]).split(':')[0]
+                    port = str(i[2]).split(':')[1]
+                    fail_list_txt.append('\nHost: ' + host_name)
+                    fail_list_txt.append('\nPort: ' + port)
+                except IndexError:
+                    host_name = str(i[2])
+                    fail_list_txt.append('\nHost: ' + host_name)
 
-            fail_list_txt.append('\nService: ' + i[3])
-            fail_list_txt.append('\nTime: ' + str(i[1]))
+                fail_list_txt.append('\nService: ' + i[3])
+                fail_list_txt.append('\nTime: ' + str(i[1]))
 
-            # Look up note if present
-            try:
-                note_txt = str([x[4] for x in server_list if x[1] == host_name][0])
-            except IndexError:
-                note_txt = ''
+                try:
+                    # Try to find note in monitor_list
+                    note_txt = str([x[4] for x in monitor_list_in if x[1] == host_name][0])
+                except IndexError:
+                    note_txt = ''
+                fail_list_txt.append('\nNote: ' + note_txt)
 
-            fail_list_txt.append('\nNote: ' + note_txt)
-            fail_list_txt.append('\n')
+                fail_list_txt.append('\n')
+            return ''.join(fail_list_txt)
 
-        logging.debug('BREAKPOINT')
+        # Start message creation here
+        fail_list = db_helpers.server_stats.failures_in_x_minutes_ago(
+            db_helpers.email_log.email_sent_x_minutes_ago())
 
+        monitor_list = db_helpers.monitor_list.get_server_list()
+
+        subj = "Server Status Report - # of failures: " + str(len(fail_list))
         # noinspection PyListCreation
-        msg = ['The First Line: \n\n']  # Email Message Body
+        msg = []  # Email Message Body
+        msg.append('Failure report: \n')
+        msg.append('# of failures: ' + str(len(fail_list)) + '\n')
+        msg.append('# of services monitored: ' + str(len(monitor_list)) + '\n')
 
-        msg.append('\nA message')
+        msg.append(generate_fail_list(fail_list, monitor_list))
 
         msg.append('\n\nReport Generated @ ' + str(datetime.now()))
-        subj = "Server Status Report"
-
+        print(''.join(msg))
+        logging.debug('BREAKPOINT')
         email_controller.send_gmail().send(subject=subj, text=''.join(msg))
-        db_helpers.email_log.log_email_sent(''.join(msg))
+        # db_helpers.email_log.log_email_sent(''.join(msg))
